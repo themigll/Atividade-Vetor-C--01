92aa0391c7f8cdd07c8cca8cc440ccf38abc8f2c
Miguel da Silva Santos <miguelzin9990@gmail.com>
2026-05-23 23:21:12 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,30 +2,28 @@
 #include <stdlib.h>
 
 int main() {
-    int v1[10];
-    int v2[10];
+    int vetor[10];
+    int pares = 0;
+    int impares = 0;
     int i;
 
     printf("Digite 10 numeros inteiros:\n");
     for(i = 0; i < 10; i++) {
-        printf("Posicao %d: ", i + 1);
-        scanf("%d", &v1[i]);
+        printf("Numero %d: ", i + 1);
+        scanf("%d", &vetor[i]);
     }
 
     for(i = 0; i < 10; i++) {
-        v2[i] = v1[9 - i];
+        if(vetor[i] % 2 == 0) {
+            pares++;
+        } else {
+            impares++;
+        }
     }
 
-    printf("\n--- Vetor 1 (Original) ---\n");
-    for(i = 0; i < 10; i++) {
-        printf("%d ", v1[i]);
-    }
-
-    printf("\n\n--- Vetor 2 (Invertido) ---\n");
-    for(i = 0; i < 10; i++) {
-        printf("%d ", v2[i]);
-    }
-    printf("\n\n");
+    printf("\n--- Resultado ---\n");
+    printf("Quantidade de numeros pares: %d\n", pares);
+    printf("Quantidade de numeros impares: %d\n\n", impares);
 
     system("pause");
 
